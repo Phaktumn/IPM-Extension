91a88d5c09880a213d41750a7f9450d817290057
monicalima18666 <monicalima@ipvc.pt>
2025-03-08 19:47:55 +0000

    update progress
    jorge és um mongo

--- content_script_docstart_MAIN.js
+++ content_script_docstart_MAIN.js
@@ -16,9 +16,15 @@ window.injectScript(
                 createElement(
                     'link',
                     {
-                        href: 'https://fonts.googleapis.com/css?family=Noto Sans',
+                        href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0',
                         rel: 'stylesheet'
                     }),
+                createElement(
+                    'link',
+                    {
+                        href: 'https://fonts.googleapis.com/css?family=Noto Sans',
+                        rel: 'stylesheet'
+                    })
             );
     }
     + "());"
